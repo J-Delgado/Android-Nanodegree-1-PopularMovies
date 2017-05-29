60c08d1cea3c660149a6923621c54edf875e9978
Javier Delgado <delga.soria@gmail.com>
2017-05-29 22:55:18 +0200

    Use AsyncTaskLoader

--- app/src/main/java/com/javierdelgado/popularmovies_stage1/activities/MainActivity.java
+++ app/src/main/java/com/javierdelgado/popularmovies_stage1/activities/MainActivity.java
@@ -21,10 +21,15 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -50,7 +55,10 @@ import butterknife.ButterKnife;
  * Created by Delga on 03/05/2017.
  */
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener, LoaderManager.LoaderCallbacks<List<Movie>> {
+
+    private static final int DOWNLOAD_MOVIES_LOADER = 22;
 
     private static final String POPULAR = "POPULAR";
     private static final String TOP = "TOP";
@@ -91,11 +99,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSwipeRefreshLayout.setOnRefreshListener( new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                new MovieDownloaderTask().execute(mLastOptionSelected);
+                Loader<List<Movie>> loader = getSupportLoaderManager().getLoader(DOWNLOAD_MOVIES_LOADER);
+                if (loader == null){
+                    getSupportLoaderManager().initLoader(DOWNLOAD_MOVIES_LOADER, null, MainActivity.this);
+                } else {
+                    getSupportLoaderManager().restartLoader(DOWNLOAD_MOVIES_LOADER, null, MainActivity.this);
+                }
             }
         });
         //
-        new MovieDownloaderTask().execute(mLastOptionSelected);
+        Loader<List<Movie>> loader = getSupportLoaderManager().getLoader(DOWNLOAD_MOVIES_LOADER);
+        if (loader == null){
+            getSupportLoaderManager().initLoader(DOWNLOAD_MOVIES_LOADER, null, this);
+        } else {
+            getSupportLoaderManager().restartLoader(DOWNLOAD_MOVIES_LOADER, null, this);
+        }
     }
 
     private void setToolbarSubtitle() {
@@ -126,11 +144,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             alertBuilder.setItems(R.array.sortings_array, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
+                    Bundle queryBundle = new Bundle();
+                    LoaderManager loaderManager = getSupportLoaderManager();
+                    Loader<List<Movie>> githubSearchLoader = loaderManager.getLoader(DOWNLOAD_MOVIES_LOADER);
                     if (i == 0){
-                        new MovieDownloaderTask().execute(POPULAR);
+                        queryBundle.putString(KEY_FILTER_SELECTED, POPULAR);
+                        if (githubSearchLoader == null) {
+                            loaderManager.initLoader(DOWNLOAD_MOVIES_LOADER, queryBundle, MainActivity.this);
+                        } else {
+                            loaderManager.restartLoader(DOWNLOAD_MOVIES_LOADER, queryBundle, MainActivity.this);
+                        }
                         mLastOptionSelected = POPULAR;
                     } else if (i == 1){
-                        new MovieDownloaderTask().execute(TOP);
+                        queryBundle.putString(KEY_FILTER_SELECTED, TOP);
+                        if (githubSearchLoader == null) {
+                            loaderManager.initLoader(DOWNLOAD_MOVIES_LOADER, queryBundle, MainActivity.this);
+                        } else {
+                            loaderManager.restartLoader(DOWNLOAD_MOVIES_LOADER, queryBundle, MainActivity.this);
+                        }
                         mLastOptionSelected = TOP;
                     } else if (i == 2){
                         mLastOptionSelected = FAVORITE;
@@ -143,66 +174,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    class MovieDownloaderTask extends AsyncTask<String, Void, Boolean>{
+    @Override
+    public Loader<List<Movie>> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<List<Movie>>(this) {
 
-        private final String popularUrl = getResources().getString(R.string.url_base_popular) + getResources().getString(R.string.API_KEY_the_movie_db);
-        private final String topRatedUrl = getResources().getString(R.string.url_base_top_rated) + getResources().getString(R.string.API_KEY_the_movie_db);
+            private final String popularUrl = getResources().getString(R.string.url_base_popular) + getResources().getString(R.string.API_KEY_the_movie_db);
+            private final String topRatedUrl = getResources().getString(R.string.url_base_top_rated) + getResources().getString(R.string.API_KEY_the_movie_db);
 
-        private List<Movie> moviesList = new ArrayList<>();
+            List<Movie> mMoviesResult;
 
-        @Override
-        protected void onPreExecute() {
-            mSwipeRefreshLayout.setRefreshing(true);
-            error.setVisibility(View.INVISIBLE);
-        }
+            @Override
+            protected void onStartLoading() {
 
-        @Override
-        protected Boolean doInBackground(String... params) {
-            URL url;
-            String selector = params[0];
-            try {
-                if (selector != null && selector.equals(TOP)){
-                    url = new URL(topRatedUrl);
-                } else {
-                    url = new URL(popularUrl);
-                }
-                URLConnection connection = url.openConnection();
-                String response = Utils.getString(connection.getInputStream());
+                mSwipeRefreshLayout.setRefreshing(true);
+                error.setVisibility(View.INVISIBLE);
                 //
-                JSONObject jsonObject = new JSONObject(response);
-                JSONArray results = jsonObject.getJSONArray("results");
-                for (int i=0; i < results.length(); i++){
-                    JSONObject item = results.getJSONObject(i);
+                if (mMoviesResult != null) {
+                    deliverResult(mMoviesResult);
+                } else {
+                    forceLoad();
+                }
+            }
 
-                    //Build item
-                    int id = item.getInt("id");
-                    String title = item.getString("title");
-                    String cover = item.getString("poster_path");
-                    String backdrop = item.getString("backdrop_path");
-                    String releaseDate = item.getString("release_date");
-                    double stars = item.getDouble("vote_average");
-                    String description = item.getString("overview");
-                    moviesList.add(new Movie(id,title,cover,backdrop,releaseDate,stars,description));
+            @Override
+            public List<Movie> loadInBackground() {
+
+                URL url;
+                String selector = args != null ? args.getString(KEY_FILTER_SELECTED) : null;
+                List<Movie> moviesList = new ArrayList<>();
+
+                try {
+                    if (selector != null && selector.equals(TOP)){
+                        url = new URL(topRatedUrl);
+                    } else {
+                        url = new URL(popularUrl);
+                    }
+                    URLConnection connection = url.openConnection();
+                    String response = Utils.getString(connection.getInputStream());
+                    //
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    for (int i=0; i < results.length(); i++){
+                        JSONObject item = results.getJSONObject(i);
+
+                        //Build item
+                        int id = item.getInt("id");
+                        String title = item.getString("title");
+                        String cover = item.getString("poster_path");
+                        String backdrop = item.getString("backdrop_path");
+                        String releaseDate = item.getString("release_date");
+                        double stars = item.getDouble("vote_average");
+                        String description = item.getString("overview");
+                        moviesList.add(new Movie(id,title,cover,backdrop,releaseDate,stars,description));
+                    }
+
+                } catch (JSONException | IOException e) {
+                    e.printStackTrace();
+                    return null;
                 }
 
-            } catch (JSONException | IOException e) {
-                e.printStackTrace();
-                return false;
+                return moviesList;
             }
 
-            return true;
-
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            mSwipeRefreshLayout.setRefreshing(false);
-            if (success) {
-                mAdapter.setData(moviesList);
-            } else {
-                mAdapter.setData(new ArrayList<Movie>());
-                error.setVisibility(View.VISIBLE);
+            // COMPLETED (3) Override deliverResult and store the data in mGithubJson
+            // COMPLETED (4) Call super.deliverResult after storing the data
+            @Override
+            public void deliverResult(List<Movie> githubJson) {
+                mMoviesResult = githubJson;
+                super.deliverResult(githubJson);
             }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Movie>> loader, List<Movie> data) {
+        mSwipeRefreshLayout.setRefreshing(false);
+        if (data != null) {
+            mAdapter.setData(data);
+        } else {
+            mAdapter.setData(new ArrayList<Movie>());
+            error.setVisibility(View.VISIBLE);
         }
     }
+
+    @Override
+    public void onLoaderReset(Loader<List<Movie>> loader) {
+
+    }
+
 }
